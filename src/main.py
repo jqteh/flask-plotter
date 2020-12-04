7a02ed12b10499fb5fe680ab104742daf96df8be
Felipe Li <felipeyenlunli@gmail.com>
2020-12-04 16:51:29 +0000

    made another error message that checks json

--- src/main.py
+++ src/main.py
@@ -6,13 +6,17 @@ app = Flask(__name__)
 @app.route('/plotter', methods=['POST'])
 def plotter():
     if request.method == 'POST':
-        data = request.json
+        if not request.is_json:
+            return jsonify({'error': 'The posted data is not JSON'})
+
+        data = request.get_json()
         print(data)
+        
         if data is None:
             return jsonify({'error':'no data'})
 
         try:
             message = plot(data)
-            return jsonify({'correct': message})
+            return jsonify({'message': message})
         except:
             return jsonify({'error':'error during calling plot function'})
--- test/test.py
+++ test/test.py
@@ -1,13 +1,11 @@
 import requests
 
-data_in_json = """
-{
+data_in_json = {
     "Felipe" : {
         "time": [27, 28, 29, 30, 31, 32, 33, 34],
         "measurements": [30, 30, 31, 31.2, 33, 33.2, 33.3, 33.4]
     }
 }
-"""
 
 resp = requests.post("http://127.0.0.1:5000/plotter", json=data_in_json)
 
