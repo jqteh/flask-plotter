1533e247944b7e5e6114f9e5556a3ac5a45e6830
Felipe Li <felipeyenlunli@gmail.com>
2020-12-06 18:03:28 +0000

    make the border tight

--- src/plot_graph.py
+++ src/plot_graph.py
@@ -13,10 +13,10 @@ def plot(data_in_json):
     try:
         img = plt.imread("src/static/gestation.png")
         fig, ax = plt.subplots()
-        plt.scatter(x, y)
+        plt.scatter(x, y, s=100 , c='r', marker='x', linewidths=1)
         ax.imshow(img, extent=[23, 42, 17, 39])
     
-        fig.savefig('src/bin/figure.png', format='png', dpi=400)
+        fig.savefig('src/bin/figure.png', bbox_inches='tight', format='png', dpi=400)
         plt.close(fig)
     except:
         return {'ERROR': 'Problem arised during plotting'}
